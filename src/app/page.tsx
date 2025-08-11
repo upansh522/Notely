0cadf77bcfdb4fa581bc8fe37868a3cbacf1c930
upansh522 <nishantupadhyay813@gmail.com>
2025-08-11 23:11:20 +0530

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -383,7 +383,7 @@ export default function LandingPage() {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t border-blue-200">
         <p className="text-xs text-gray-500 dark:text-gray-400">
-          © 2024 Notely.ai. All rights reserved.
+          © 2025 Notely.ai. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
