6945fdec49a393ae8cbd8655ca8e3be6064966b4
[Nishant Upadhyay] <nishantupadhyay813@gmail.com>
2026-02-21 18:20:46 +0530

    correct the payment gateway

--- src/app/dashboard/upgrade/page.tsx
+++ src/app/dashboard/upgrade/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { useUser } from "@clerk/nextjs";
 import axios from "axios";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import { toast } from "sonner";
 
 interface paymentIntentType {
   data: {
@@ -15,215 +17,161 @@ const page = () => {
   const plan = useQuery(api.user.fetchUserPlan, {
     email: user?.primaryEmailAddress?.emailAddress as string,
   });
-  const initPayment = async () => {
-    const paymentIntent: paymentIntentType = await axios.post("/api/upgrade", {
-      email: user?.primaryEmailAddress?.emailAddress,
+  const userUpgradePlan= useMutation(api.user.updateUserPlan)
+  const onPaymentSuccess = async () => {
+    await userUpgradePlan({
+      email: user?.primaryEmailAddress?.emailAddress as string,
     });
-    if (paymentIntent.data.message === "success") {
-      window.location.href = paymentIntent.data.url;
-    }
+    console.log("payment success");
+    toast("payment success",{
+      description:"your plan has been upgraded successfully",
+      
+    })
+    
   };
   return (
     <div className="p-10">
       <h2 className="text-3xl font-bold">Plans</h2>
       <p>Upgrade your plan to store unlimited notes</p>
       <div className="mx-auto max-w-3xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8">
-          <div className="rounded-2xl border border-indigo-600 p-6 shadow-sm ring-1 ring-indigo-600 sm:order-last sm:px-8 lg:p-12">
-            <div className="text-center">
-              <h2 className="text-lg font-medium text-gray-900">
-                Unlimited
-                <span className="sr-only">Plan</span>
-              </h2>
+  <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 sm:items-center md:gap-8">
+    <div className="rounded-2xl border border-indigo-600 p-6 shadow-xs ring-1 ring-indigo-600 sm:order-last sm:px-8 lg:p-12">
+      <div className="text-center">
+        <h2 className="text-lg font-medium text-gray-900">
+          unlimited
+          <span className="sr-only">Plan</span>
+        </h2>
 
-              <p className="mt-2 sm:mt-4">
-                <span className="text-3xl font-medium text-gray-700">
-                  9$ / one time
-                </span>
-              </p>
-            </div>
+        <p className="mt-2 sm:mt-4">
+          <strong className="text-3xl font-bold text-gray-900 sm:text-4xl"> 30$ </strong>
 
-            <ul className="mt-6 space-y-2">
-              <li className="flex items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-indigo-700"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12.75l6 6 9-13.5"
-                  />
-                </svg>
-
-                <span className="text-gray-700"> Unlimited PDF upload </span>
-              </li>
-
-              <li className="flex items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-indigo-700"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12.75l6 6 9-13.5"
-                  />
-                </svg>
-
-                <span className="text-gray-700"> Unlimited Note Taking </span>
-              </li>
-
-              <li className="flex items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-indigo-700"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12.75l6 6 9-13.5"
-                  />
-                </svg>
-
-                <span className="text-gray-700"> Email support </span>
-              </li>
-
-              <li className="flex items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-indigo-700"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12.75l6 6 9-13.5"
-                  />
-                </svg>
-
-                <span className="text-gray-700"> Help center access </span>
-              </li>
-            </ul>
-
-            <button
-              onClick={initPayment}
-              disabled={plan === "unlimited"}
-              className="mt-8 mx-auto block rounded-full border border-indigo-600 bg-indigo-600 px-12 py-3 text-center text-sm font-medium text-white hover:bg-indigo-700 hover:ring-1 hover:ring-indigo-700 focus:outline-none focus:ring active:text-indigo-500"
-            >
-              {plan === "unlimited" ? "Current plan" : "Get Started"}
-            </button>
-          </div>
-
-          <div className="rounded-2xl border border-gray-200 p-6 shadow-sm sm:px-8 lg:p-12">
-            <div className="text-center">
-              <h2 className="text-lg font-medium text-gray-900">
-                Free
-                <span className="sr-only">Plan</span>
-              </h2>
-
-              <p className="mt-2 sm:mt-4">
-                <span className="text-3xl font-medium text-gray-700">
-                  0$ / month
-                </span>
-              </p>
-            </div>
-
-            <ul className="mt-6 space-y-2">
-              <li className="flex items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-indigo-700"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12.75l6 6 9-13.5"
-                  />
-                </svg>
-
-                <span className="text-gray-700"> 5 PDF upload</span>
-              </li>
-
-              <li className="flex items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-indigo-700"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12.75l6 6 9-13.5"
-                  />
-                </svg>
-
-                <span className="text-gray-700"> Unlimited note taking </span>
-              </li>
-
-              <li className="flex items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-indigo-700"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12.75l6 6 9-13.5"
-                  />
-                </svg>
-
-                <span className="text-gray-700"> Email support </span>
-              </li>
-
-              <li className="flex items-center gap-1">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  className="size-5 text-indigo-700"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4.5 12.75l6 6 9-13.5"
-                  />
-                </svg>
-
-                <span className="text-gray-700"> Help center access </span>
-              </li>
-            </ul>
-          </div>
-        </div>
+          <span className="text-sm font-medium text-gray-700">/month</span>
+        </p>
       </div>
+
+      <ul className="mt-6 space-y-2">
+        <li className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 text-indigo-700">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+          </svg>
+
+          <span className="text-gray-700"> 20 users included </span>
+        </li>
+
+        <li className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 text-indigo-700">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+          </svg>
+
+          <span className="text-gray-700"> 5GB of storage </span>
+        </li>
+
+        <li className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 text-indigo-700">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+          </svg>
+
+          <span className="text-gray-700"> Email support </span>
+        </li>
+
+        <li className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 text-indigo-700">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+          </svg>
+
+          <span className="text-gray-700"> Help center access </span>
+        </li>
+
+        <li className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 text-indigo-700">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+          </svg>
+
+          <span className="text-gray-700"> Phone support </span>
+        </li>
+
+        <li className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 text-indigo-700">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+          </svg>
+
+          <span className="text-gray-700"> Community access </span>
+        </li>
+      </ul>
+      <div className="mt-8">
+        <PayPalButtons 
+        onApprove={()=>onPaymentSuccess()}
+        onCancel={()=> console.log('payment cancelled')}
+        createOrder={(data,actions)=>{
+          return actions?.order?.create({
+            purchase_units:[
+              {
+                amount:{
+                  value:30,
+                  currency_code:'USD'               
+                }
+              } 
+            ]
+          })
+        }}
+        />
+      </div>
+    </div>
+
+    <div className="rounded-2xl border border-gray-200 p-6 shadow-xs sm:px-8 lg:p-12">
+      <div className="text-center">
+        <h2 className="text-lg font-medium text-gray-900">
+          Free 
+          <span className="sr-only">Plan</span>
+        </h2>
+
+        <p className="mt-2 sm:mt-4">
+          <strong className="text-3xl font-bold text-gray-900 sm:text-4xl"> 0$ </strong>
+
+          <span className="text-sm font-medium text-gray-700">/month</span>
+        </p>
+      </div>
+
+      <ul className="mt-6 space-y-2">
+        <li className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 text-indigo-700">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+          </svg>
+
+          <span className="text-gray-700"> 10 users included </span>
+        </li>
+
+        <li className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 text-indigo-700">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+          </svg>
+
+          <span className="text-gray-700"> 2GB of storage </span>
+        </li>
+
+        <li className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 text-indigo-700">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+          </svg>
+
+          <span className="text-gray-700"> Email support </span>
+        </li>
+
+        <li className="flex items-center gap-1">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-5 text-indigo-700">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5"></path>
+          </svg>
+
+          <span className="text-gray-700"> Help center access </span>
+        </li>
+      </ul>
+
+      <a href="#" className="mt-8 block rounded-full border border-indigo-600 bg-white px-12 py-3 text-center text-sm font-medium text-indigo-600 hover:ring-1 hover:ring-indigo-600">
+        Get Started
+      </a>
+    </div>
+  </div>
+</div>
     </div>
   );
 };
--- src/providers/ConvexClientProvider.tsx
+++ src/providers/ConvexClientProvider.tsx
@@ -2,9 +2,14 @@
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return <ConvexProvider client={convex}>
+    <PayPalScriptProvider options={{ clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID! }}>
+      {children}
+    </PayPalScriptProvider>
+  </ConvexProvider>;
 }
